2a1c7493d8a6e3456b1d2f40771f696cfe03ef56
shaneski <shane@micronetclub.co.jp>
2023-02-20 17:09:20 +0900

    update score display

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, watch, ref, toRefs } from 'vue'
 
 import PersonIcon from './PersonIcon.vue'
 import RobotIcon from './RobotIcon.vue'
@@ -19,6 +19,34 @@ const props = defineProps({
   }
 })
 
+const flagAdd = ref(false)
+const flagSub = ref(false)
+
+const { score } = toRefs(props)
+
+watch(score, (value, prev) => {
+
+  //console.log("watch", score.value, value, prev)
+
+  if(prev > value) {
+    flagAdd.value = false
+    flagSub.value = true
+  } else if(prev < value) {
+    flagAdd.value = true
+    flagSub.value = false
+  }
+
+  if(flagAdd.value || flagSub.value) {
+
+    setTimeout(() => {
+      flagAdd.value = false
+      flagSub.value = false
+    }, 400)
+
+  }
+
+})
+
 const displayScore = computed(() => String(props.score).padStart(5, 0))
 
 </script>
@@ -29,7 +57,7 @@ const displayScore = computed(() => String(props.score).padStart(5, 0))
       <PersonIcon v-if="props.iconType === 'human'" size="100%" />
       <RobotIcon v-else size="100%" />
     </div>
-    <div class="score-text">{{ displayScore }}</div>
+    <div class="score-text" :class="{plus: flagAdd, minus: flagSub}">{{ displayScore }}</div>
     <div v-if="props.iconPosition === 'right'" class="score-icon">
       <PersonIcon v-if="props.iconType === 'human'" size="100%" />
       <RobotIcon v-else size="100%" />
@@ -52,9 +80,28 @@ const displayScore = computed(() => String(props.score).padStart(5, 0))
   overflow: hidden;
 }
 .score-text {
+  font-family: monospace;
   font-size: 1.5rem;
   font-weight: 600;
   transition: all 0.1s ease-in;
+}
+.plus {
+  color: #67AEFF;
+  animation: addAnim 0.3s ease-in forwards;
+}
+.minus {
+  color: #ff6767;
+  animation: subAnim 0.3s ease-in forwards;
+}
+
+@keyframes addAnim {
+  0% { color: #67AEFF; }
+  100% { color: var(--color-text); }
+}
+
+@keyframes subAnim {
+  0% { color: #ff6767; }
+  100% { color: var(--color-text); }
 }
 </style>
 
